02c77ed309f9bc8859f80f88c2d55c12cb5c118d
Kazuki UCHIDA <gioext@gmail.com>
2009-06-02 01:27:37 +0900

    thread

--- pic2ch-sinatra.rb
+++ pic2ch-sinatra.rb
@@ -5,7 +5,15 @@ require 'sequel'
 
 DB = Sequel.connect('sqlite://test.db')
 
+configure do
+  set :picurl, "/strage"
+end
+
 helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+  alias_method :u, :escape
+
   def parts_board_list
     @parts_board_list_boards = DB[:boards].reverse_order(:id).limit(10).all
     erb "parts/board_list".to_sym
@@ -22,6 +30,10 @@ helpers do
   def parts_footer
     erb "parts/footer".to_sym
   end
+
+  def picurl
+    options.picurl
+  end
 end
 
 get '/' do
@@ -32,4 +44,20 @@ get '/thread/:id' do
   @board = DB[:boards][:id => params[:id]]
   @pictures = DB[:pictures].filter(:board_id => params[:id]).all
   erb :thread
+end
+
+get '/feed' do
+end
+
+configure :production do
+  not_found do
+    '<div>404</div><div><a href="/">TOP</a></div>' 
+  end
+  error do
+    '<div>500</div><div><a href="/">TOP</a></div>' 
+  end
+end
+
+get '/a' do
+  options.picurl
 end
---
+++ public/pic2ch.js
@@ -0,0 +1,176 @@
+pic2ch = {
+    isIE: /*@cc_on!@*/0,
+    addEvent: function(elm, e, func) {
+        if (elm.addEventListener) {
+            elm.addEventListener(e, func, false);
+        } else if (elm.attachEvent) {
+            elm.attachEvent('on' + e, func);
+        }
+    },
+    click: function(elm, func) {
+        this.addEvent('click');       
+    },
+    load: function(elm, func) {
+        this.addEvent('load');       
+    }
+};
+
+pic2ch.$ = (function() {
+    var _doc = document;
+    return {
+        s: function(id) {
+            return _doc.getElementById(id);
+        },
+        c: function(tag) {
+            return _doc.createElement(tag);
+        }
+    };
+})();
+
+pic2ch.addStar = (function(add, star, id, count) {
+    var img10 = new Image();
+    img10.src = '/images/star10.gif';
+    var img = new Image();
+    img.src = '/images/star.gif';
+
+    return function(add, star, id, count) {
+        var star10 = Math.floor(count / 10);
+        var starn = count % 10;
+        var estar = pic2ch.$.s(star);
+        var eadd = pic2ch.$.s(add);
+
+        eadd.onclick = function() {
+            pic2ch.get('/star/' + id, function(){
+                estar.appendChild(img.cloneNode(false));
+                eadd.onclick = function() {}
+            }); 
+        };
+        for (var i = 0; i < star10; i++) {
+            estar.appendChild(img10.cloneNode(false));
+        }
+        for (var i = 0; i < starn; i++) {
+            estar.appendChild(img.cloneNode(false));
+        }
+    }
+})();
+
+pic2ch.slideshow = (function() {
+    var urls = [];
+    var container;
+    var parent_path;
+    var position = -5;
+    var cleft, cright, cimage, cthumbs, cthumb1, cthumb2, cthumb3, cthumb4, cthumb5;
+    var overlay;
+    var loading;
+    var thumbs = {};
+
+    var createUI = function() {
+        container.innerHTML = '<div id="overlay"></div><div id="slide_left">◀</div><div id="slide_right">▶</div><div id="slide_image"></div><div id="loading"></div><div id="slide_thumbs"><div id="thumb1"></div><div id="thumb2"></div><div id="thumb3"></div><div id="thumb4"></div><div id="thumb5"></div></div>';
+        cleft = pic2ch.$.s('slide_left');
+        cright = pic2ch.$.s('slide_right');
+        cimage = pic2ch.$.s('slide_image');
+        cthumbs = pic2ch.$.s('slide_thumbs');
+        thumbs.thumb1 = pic2ch.$.s('thumb1');
+        thumbs.thumb2 = pic2ch.$.s('thumb2');
+        thumbs.thumb3 = pic2ch.$.s('thumb3');
+        thumbs.thumb4 = pic2ch.$.s('thumb4');
+        thumbs.thumb5 = pic2ch.$.s('thumb5');
+        overlay = pic2ch.$.s('overlay');
+        loading = pic2ch.$.s('loading');
+
+        cright.onclick = function() {
+            pic2ch.slideshow.next();
+        };
+        cleft.onclick = function() {
+            pic2ch.slideshow.prev();
+        };
+    };
+
+    var showImage = function(n) {
+        var m = position + n;
+        if (m < 0) {
+            position = urls.length + m;
+        } else {
+            position += n;
+        }
+        for (var i = 0; i < 5; i++) {
+            var img = new Image();
+            img.style.visibility = 'hidden';
+            img.onload = (function(i, img) {
+                return function() {
+                    img.style.top = ((120 - img.height) / 2) + 'px';
+                    img.style.left = ((120 - img.width) / 2) + 'px';
+                    img.style.visibility = 'visible';
+                }
+            })(i, img);
+            img.onclick = (function(i) {
+                return function(){
+                    overlay.style.visibility = 'visible';
+                    loading.style.visibility = 'visible';
+                    var img = new Image();
+                    img.style.visibility = 'hidden';
+                    img.onload = function() {
+                        var w = img.width;
+                        var h = img.height;
+                        img.parentNode.style.marginTop = -(h / 2) + 'px';
+                        img.parentNode.style.marginLeft = -(w / 2) + 'px';
+                        img.style.visibility = 'visible';
+                        cimage.style.visibility = 'visible';
+                        loading.style.visibility = 'hidden';
+                    };
+                    img.onclick = function() {
+                        cimage.style.visibility = 'hidden';
+                        overlay.style.visibility = 'hidden';
+                        cimage.removeChild(img);
+                    };
+                    cimage.appendChild(img);
+                    img.src = '/images/pics/' + id + urls[(position + i) % urls.length] + '.jpg'; 
+                }
+            })(i);
+            if (thumbs['now' + (i + 1)]) {
+                thumbs['thumb' + (i + 1)].removeChild(thumbs['now' + (i + 1)]);
+            }
+            thumbs['thumb' + (i + 1)].appendChild(img);
+            thumbs['now' + (i + 1)] = img;
+            img.src = '/images/thumbs/' + id + urls[(position + i) % urls.length] + '.jpg';
+        }
+    };
+
+    return {
+        init: function(settings) {
+            urls = settings.urls || [];
+            container = pic2ch.$.s(settings.container) || document;
+            id = settings.id || '';
+            createUI();
+            this.next();
+        },
+        next: function() {
+            showImage(5);
+        },
+        prev: function() {
+            showImage(-5);
+        }
+    };
+})();
+pic2ch.get = function(url, callback) {
+    var req = null;
+    try {
+        if (window.XMLHttpRequest) {
+            req = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            try {
+                req = new ActiveXObject('Msxml2.XMLHTTP');
+            } catch(e) {
+                req = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+        }
+    } catch(e) {}
+    if (!req) return;
+    req.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText);
+        }
+    };
+    req.open('get', url, true);
+    req.send(null);
+};
